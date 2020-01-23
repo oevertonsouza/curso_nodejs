e9413dfc06c38ab19ffb9c95bf59c2d63f273993
Everton <oevertonsouza@gmail.com>
2020-01-23 17:15:55 -0300

    Atualizando views

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send('<html><body>Portal de qualquer coisa</body></html>')
+    res.render('home/index');
 });
 
-app.get('/tecnologia', function(req, res){
-    res.render('secao/tecnologia');
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia');
 });
 
-app.get('/moda', function(req, res){
-    res.send('<html><body>Portal de moda</body></html>');
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias');
 });
 
 app.listen(3000, function(){
